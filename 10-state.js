b39387b2f7d8ebddb93b4f4dfe4d6e9ee4b3371c
limingru <18813070947@163.com>
2019-09-06 14:28:51 +0800

    手写一个简单的Promise

--- 10-state.js
+++ 10-state.js
@@ -1,9 +1,11 @@
+import StateMachine from 'javascript-state-machine'
+
 /*
  * 1. 一个对象有状态变化，每次状态变化都会触发一个逻辑，而且不能总是用if...else来控制
  * 2. 感性理解：红绿灯，示例代码如下
  * 3. 应用：
- *   3.1 有限状态机
- *   3.2 写一个简单的Promise
+ *   3.1 有限状态机，使用javascript-state-machine
+ *   3.2 写一个简单的Promise，代码如下：
  */
 
 // 存储状态，红灯、黄灯、绿灯
@@ -11,6 +13,7 @@ class State {
   constructor(color) {
     this.color = color
   }
+
   handle(context) {
     console.log(`已经切换到了${this.color}`)
     context.setState(this)
@@ -33,7 +36,7 @@ class Context {
   }
 }
 
-const context = new Context();
+const context = new Context()
 
 const red = new State('red')
 const green = new State('green')
@@ -46,3 +49,83 @@ console.log(`当前状态为${context.getState().color}`)
 yellow.handle(context)
 console.log(`当前状态为${context.getState().color}`)
 
+/*
+ * 安装javascript-state-machine然后再运行下面的代码
+ */
+const fsm = new StateMachine({
+  init: '收藏',
+  transition: [
+    {
+      name: 'doStore',
+      from: '收藏',
+      to: '取消收藏'
+    },
+    {
+      name: 'deleteStore',
+      from: '取消收藏',
+      to: '收藏'
+    }
+  ],
+  methods: {
+    // 监听doStore
+    onDoStore() {
+      console.log('收藏成功')
+    },
+    // 监听deleteStore
+    onDeleteStore() {
+      console.log('取消收藏')
+    }
+  }
+})
+
+const div = document.getElementById('btn')
+div.onclick = function () {
+  if (fsm.is('收藏')) {
+    fsm.doStore()
+  } else {
+    fsm.deleteStore()
+  }
+}
+
+/*
+ * 写一个简单的Promise
+ */
+const fsm2 = new StateMachine({
+  init: 'pending',
+  transitions: [
+    {
+      name: 'resolve',
+      from: 'pending',
+      to: 'fullfilled'
+    },
+    {
+      name: 'reject',
+      from: 'pending',
+      to: 'rejected'
+    }
+  ],
+  methods: {
+    onResolve(state, data) {
+      data.successList.forEach(fn => fn())
+    },
+    onReject(state, data) {
+      data.failList.forEach(fn => fn())
+    }
+  }
+})
+
+class MyPromise {
+  constructor(fn) {
+    this.successList = []
+    this.failList = []
+    fn(function resolve() {
+      fsm.resolve(this)
+    }, function reject() {
+      fsm.reject(this)
+    })
+  }
+  then(successFn, rejectFn) {
+    this.successList.push(successFn)
+    this.failList.push(rejectFn)
+  }
+}
